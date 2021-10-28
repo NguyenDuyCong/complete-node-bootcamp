ef9a240d05d6232df498bd185fea9a4a224577d0
NguyenDuyCong <duycong.cn@gmail.com>
2021-10-28 14:41:47 +0700

    create simple api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const http = require('http');
 
+////////////////////////////////
+// FILES
 // const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textInput);
 
@@ -7,9 +10,39 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!')
 
-// TODO: asynchronous or non-blocking code execution
-fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
-  console.log(data);
+// // TODO: asynchronous or non-blocking code execution
+// fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+
+// console.log("Reading file...");
+
+//////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/') {
+    res.end("hello from server");
+  } else if (pathName === "/api"){
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {
+        'Content-type': 'application/json'
+      });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!!!</h1>')
+  }
 });
 
-console.log("Reading file...");
+server.listen(8000, 'localhost', () => {
+  console.log("Listening to request on port 8000");
+})
+
