182b59c6b7f82dbf7c56b6c2a4033eb9ff7435d9
NguyenDuyCong <duycong.cn@gmail.com>
2021-10-28 13:15:29 +0700

    non-blocking code execution

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textInput);
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textInput);
 
-const textOut = `Duplicate text: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!')
+// const textOut = `Duplicate text: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!')
+
+// TODO: asynchronous or non-blocking code execution
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+
+console.log("Reading file...");
