95a1e18dc46015a0f81f1639e93abb6669c79a7f
NguyenDuyCong <duycong.cn@gmail.com>
2021-10-29 17:38:03 +0700

    use template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ////////////////////////////////
 // FILES
@@ -19,20 +20,58 @@ const http = require('http');
 
 //////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  
+  if (!product.organic) {
+    output= output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  // console.log(output);
+  return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const {query, pathname} = url.parse(req.url, true);
+  
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    
+    res.writeHead(200, {'Content-type': 'text/html'});
 
-  if (pathName === '/') {
-    res.end("hello from server");
-  } else if (pathName === "/api"){
-    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, {
-        'Content-type': 'application/json'
-      });
-      res.end(data);
-    });
+    // console.log(dataObj);
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    // console.log(cardsHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+  // Product page 
+  } else if (pathname === "/product"){
+    
+    res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+  // api
+  } else if (pathname == '/api') {
+    res.writeHead(200, {'Content-type': 'application/json'});
+    res.end(data);
+
+  // 404 page
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
@@ -42,7 +81,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, 'localhost', () => {
+server.listen(8000, '127.0.0.1', () => {
   console.log("Listening to request on port 8000");
 })
 
